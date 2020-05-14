9cb035ba5d8f762c3ff5c5beaf540d11f9dab6bc
John Clayton <john@fivesquare.net>
2020-05-14 13:18:11 -0400

    Initial Commit

---
+++ FashionAroundMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FashionAroundMe
+//
+//  Created by John Clayton on 2020/5/14.
+//  Copyright © 2020 John Clayton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
